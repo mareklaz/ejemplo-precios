9a8a216de920b8e559b994fb6d8c64ba2b4eca07
Marek Laz <mareklaz4@gmail.com>
2022-11-15 01:02:13 +0100

    client crud

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -1,18 +1,37 @@
 const Cliente = require('../models/Cliente.model');
 
-module.exports.list {
-
-}
-module.exports.detail {
-  
-}
-module.exports.create {
-  
-}
-module.exports.update {
-  
-}
-module.exports.delete {
-  
-}
-
+module.exports.list = (req, res, next) => {
+  Cliente.find()
+    .then((clientes) => {
+      res.json(clientes);
+    })
+    .catch(next);
+};
+module.exports.detail = (req, res, next) => {
+  const { id } = req.params;
+  Cliente.findById(id)
+    .then((cliente) => {
+      res.json(cliente);
+    })
+    .catch(next);
+};
+module.exports.create = (req, res, next) => {
+  Cliente.create(req.body)
+    .then((cliente) => res.status(201).json(cliente))
+    .catch(next);
+};
+module.exports.update = (req, res, next) => {
+  const { id } = req.body;
+  Cliente.findOneAndUpdate(id, req.body, { new: true, rawResult: true })
+    .then((cliente) => {
+      res.status(200).json(cliente);
+    })
+    .catch(next);
+};
+module.exports.delete = (req, res, next) => {
+  console.log(req.body);
+  const { id } = req.body;
+  Cliente.findByIdAndDelete(id)
+    .then(() => res.status(204))
+    .catch(next);
+};
